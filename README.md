96e79704b34917084f1116ec395b356abd1ef627
Joseph Mokenela <joseph.mokenela@gmail.com>
2016-07-23 22:49:48 +0200

    First commit of the firebase-android demo

---
+++ README.md
@@ -0,0 +1,4 @@
+
+#  A simple prototype to demonstrate the use of  Android and Firebase
+
+#  The idea here is to get accostomed to Firebase backend and how to integrate it with Android
---
+++ app/src/main/java/com/mmjmicrosystems/firebase/shoppingcard/ShoppingListApplication.java
@@ -0,0 +1,19 @@
+package com.mmjmicrosystems.firebase.shoppingcard;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /* Initialize Firebase */
+        Firebase.setAndroidContext(this);
+        /* Enable disk persistence  */
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
+    }
+
+}
